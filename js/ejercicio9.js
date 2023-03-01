c7078a0b07e16b361278caf04a8ac9e275fcb1c4
RLauraS <R_LauraS@yahoo.com.ar>
2023-03-01 20:48:21 -0300

    Se realiza ejercicio 9

--- js/ejercicio9.js
+++ js/ejercicio9.js
@@ -1,2 +1,8 @@
 /* 9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
 Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)*/
+let frase = prompt("Por favor,escriba una frase o palabra");
+let longitud = (frase.length);
+for(let i=0;i<=longitud-1;i=i+1){
+if (frase.charAt(i) === 'a'|| frase.charAt(i) ==='e'||frase.charAt(i) ==='i'||frase.charAt(i) ==='o'|| frase.charAt(i) ==='u'||frase.charAt(i) === 'A'|| frase.charAt(i)==='E'||frase.charAt(i) ==='I'||frase.charAt(i) ==='O'|| frase.charAt(i) ==='U'){
+    document.write(frase.charAt(i));}
+}
